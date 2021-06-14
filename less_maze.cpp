707023074f371854fd345216d1ec6e4fe5185e4b
TalandBotirjon <85695336+TalandBotirjon@users.noreply.github.com>
2021-06-14 20:01:15 +0500

    ASCII kod orqali labirint.

---
+++ less_maze.cpp
@@ -0,0 +1,70 @@
+/*
+C++ dasturlash tilida ASCII kodlardan foydalanib kichik bir labirint.
+#define bb <<(char)201 ko'rinishda yozishganlaridan maqsad.
+Men bb ni asosiy funksiyada(int main()) ichida yozganimda "<<(char)201" 
+buyrug'ini bajaradi.
+Bu bilan men yozgan labirint kodimni qisqartirish va adashmaslik uchun yozganman. 
+
+Agar kodga tushinmasangiz murojat uchun. https://t.me/Talandbek telegram lichkamga murojjat qilishingiz mumkin.
+*/
+#include <iostream>
+#define aa <<(char)32
+#define bb <<(char)201
+#define cc <<(char)205
+#define dd <<(char)187
+#define ee <<(char)186
+#define ff <<(char)200
+#define gg <<(char)188
+#define hh <<(char)203
+#define tt <<(char)202
+#define ii <<(char)204
+#define jj <<(char)185
+#define kk <<(char)206
+#define a1 <<(char)218
+#define a2 <<(char)196
+#define a3 <<(char)191
+#define a4 <<(char)32<<(char)32<<(char)179<<(char)32<<(char)32
+#define a5 <<(char)32<<(char)32<<(char)192
+#define a6 <<(char)217
+#define a7 <<"\n";
+#define b4 <<(char)32<<(char)32<<(char)179<<(char)45<<(char)62
+#define c4 <<(char)64
+using namespace std;
+int main ()
+{
+	cout<<"\n\n"<<"         -> bu belgidan kirib  @ bunisidan chiqish yo'lini toping :)   "<<"\n\n";
+	cout aa aa a1 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a3 a7
+	cout a4 cc cc cc cc cc cc cc cc hh cc cc cc hh cc cc cc hh cc cc cc cc cc cc cc hh cc cc cc dd a4 a7
+	cout b4 aa aa aa aa aa aa aa aa ee aa aa aa ee aa aa aa ee aa aa aa aa aa aa aa ee aa aa aa ee a4 a7
+	cout a4 ee aa bb cc cc aa ee aa ee aa ee aa ee aa ee aa ee aa bb cc cc cc cc aa ee aa ee aa ee a4 a7
+	cout a4 ee aa ee aa aa aa ee aa aa aa ee aa ee aa ee aa aa aa ee aa aa aa aa aa ee aa ee aa ee a4 a7
+	cout a4 ee aa ee aa cc cc tt cc cc cc jj aa ee aa ff cc cc cc jj aa cc cc cc cc gg aa ee aa ee a4 a7
+	cout a4 ee aa ee aa aa aa aa aa aa aa ee aa ee aa aa aa aa aa ee aa aa aa aa aa aa aa ee aa ee a4 a7
+	cout a4 ee aa ii cc hh cc cc cc dd aa ee aa ff cc cc cc cc aa ii cc cc cc cc cc cc cc gg aa ee a4 a7
+	cout a4 ee aa ee aa ee aa aa aa ee aa ee aa aa aa aa aa aa aa ee aa aa aa aa aa aa aa aa aa ee a4 a7
+	cout a4 ee aa ee aa ee aa ee aa ee aa ii cc cc cc hh cc cc cc gg aa bb cc cc cc cc cc dd aa ee a4 a7
+	cout a4 ee aa ee aa ee aa ee aa ee aa ee aa aa aa ee aa aa aa aa aa ee aa aa aa aa aa ee aa ee a4 a7
+	cout a4 ee aa ee aa ee aa ee aa ee aa ee aa ee aa ee aa bb cc cc cc jj aa cc cc hh cc gg aa ee a4 a7
+	cout a4 ee aa aa aa ee aa ee aa aa aa ee aa ee aa ee aa ee aa aa aa ee aa aa aa ee aa aa aa ee a4 a7
+	cout a4 ii cc cc aa ee aa ff cc cc cc gg aa ee aa ee aa ee aa ee aa ii cc cc aa ee aa cc cc jj a4 a7
+	cout a4 ee aa aa aa ee aa aa aa aa aa aa aa ee aa ee aa aa aa ee aa ee aa aa aa ee aa aa aa ee a4 a7
+	cout a4 ee aa cc cc kk cc cc cc dd aa cc cc gg aa ii cc cc cc gg aa ee aa bb cc tt cc cc aa ee a4 a7
+	cout a4 ee aa aa aa ee aa aa aa ee aa aa aa aa aa ee aa aa aa aa aa ee aa ee aa aa aa aa aa ee a4 a7
+	cout a4 ii cc cc aa ee aa ee aa ii cc cc cc cc aa ee aa bb cc cc cc jj aa ee aa cc cc cc cc jj a4 a7
+	cout a4 ee aa aa aa ee aa ee aa ee aa aa aa aa aa ee aa ee aa aa aa ee aa ee aa aa aa aa aa ee a4 a7
+	cout a4 ee aa cc cc gg aa ee aa ee aa cc cc hh cc jj aa ee aa ee aa ee aa ii cc cc cc dd aa ee a4 a7
+	cout a4 ee aa aa aa aa aa ee aa ee aa aa aa ee aa ee aa aa aa ee aa aa aa ee aa aa aa ee aa ee a4 a7
+	cout a4 ii cc hh cc cc cc jj aa ii cc cc aa ee aa ff cc cc cc tt cc dd aa ee aa ee aa ee aa ee a4 a7
+	cout a4 ee aa ee aa aa aa ee aa ee aa aa aa ee aa aa aa aa aa aa aa ee aa aa aa ee aa ee aa ee a4 a7
+	cout a4 ee aa ee aa ee aa ee aa ee aa cc cc jj aa cc cc cc cc dd aa ii cc cc cc jj aa ee aa ee a4 a7
+	cout a4 ee aa ee aa ee aa aa aa ee aa aa aa ee aa aa aa aa aa ee aa ee aa aa aa ee aa ee aa ee a4 a7
+	cout a4 ee aa ee aa ff cc cc cc tt cc cc aa ff cc cc cc cc aa ee aa ee aa ee aa ee aa ee aa ee a4 a7
+	cout b4 aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa aa ee aa aa aa ee aa aa aa ee aa c4 a4 a7
+	cout a4 cc cc cc cc cc cc cc cc cc cc cc cc cc cc cc cc cc cc tt cc cc cc tt cc cc cc tt cc cc a4 a7
+	cout a5 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a2 a6;
+	cout <<"\n\n";
+	cout << "      yo'l topgan bo'lsangiz yaxshi \n      Agar topolmasangiz hech qisi yo'q yana qaytadan harakat qiling  \n";
+  
+  return 0;
+  
+}
